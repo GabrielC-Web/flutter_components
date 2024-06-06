d35c1106fa3c3ec976c071d9964e5123791642eb
gcarreno <gcarreno@pagochinchin.com>
2024-06-06 17:00:09 -0400

    creada lista de strings

--- lib/screens/listview1_screen.dart
+++ lib/screens/listview1_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class ListView1Screen extends StatelessWidget {
   const ListView1Screen({super.key});
 
+  final options = const ['Lizzie', 'Wiley', 'Jesus', 'Celeste', 'Mathilde'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,7 +12,17 @@ class ListView1Screen extends StatelessWidget {
           title: const Text('Listview tipo 1'),
         ),
         body: ListView(
-          children: const [Text('Hello, there')],
+          children: [
+            ...options.map((e) => Column(
+                  children: [
+                    ListTile(
+                      title: Text(e),
+                      trailing: const Icon(Icons.arrow_forward_outlined),
+                    ),
+                    const Divider()
+                  ],
+                ))
+          ],
         ));
   }
 }
