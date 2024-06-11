a8f893f0a53e47022e3b3e0ad7ed9e862e99fc95
gcarreno <gcarreno@pagochinchin.com>
2024-06-11 16:38:40 -0400

    creados menuOptions

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -1,9 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components_1/models/menu_option.dart';
 
 import '../screens/screens.dart';
 
 class AppRoutes {
   static const initialRoute = 'home';
+
+  static final menuOptions = <MenuOption>[
+    // TODO: borrar home
+    MenuOption(
+        route: 'home',
+        icon: Icons.home,
+        name: 'Home Screen',
+        screen: const HomeScreen()),
+    MenuOption(
+        route: 'listview1',
+        icon: Icons.home,
+        name: 'Lista 1',
+        screen: const ListView1Screen()),
+    MenuOption(
+        route: 'listview2',
+        icon: Icons.home,
+        name: 'Lista 2',
+        screen: const ListView2Screen()),
+    MenuOption(
+        route: 'card',
+        icon: Icons.home,
+        name: 'Card',
+        screen: const CardScreen()),
+    MenuOption(
+        route: 'alet',
+        icon: Icons.home,
+        name: 'Alert',
+        screen: const AlertScreen()),
+  ];
 
   static Map<String, Widget Function(BuildContext)> routes = {
     'home': (BuildContext context) => const HomeScreen(),
